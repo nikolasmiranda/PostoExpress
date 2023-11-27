49298edd7739c48f8c025df5f51f58fb9bd71540
nikolasmiranda <129020080+nikolasmiranda@users.noreply.github.com>
2023-11-27 09:43:59 -0300

    Create index.php
    Criação do Html

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="script.js" defer></script>
+</head>
+<body>
+    <section id=navegacao>
+    <nav>
+        <ul id="ulnav">
+            <li class="navli"><a class="ancoragemnav" href="index.html">Início</a></li>
+            <li class="navli"><a class="ancoragemnav" href="calculo-gasolina.php">Calculo de Gasolina</a></li>
+            <li class="navli"><a class="ancoragemnav" href="projetoAplicacoesParaInternet.html#definicao">Unidades</a></li>
+            <li class="navli"><a class="ancoragemnav" href="projetoAplicacoesParaInternet.html#rodape">Sobre</a></li>
+            <li class="navli"><a class="ancoragemnav" href="contato.html">Contato</a></li>
+        </ul>
+    </nav>
+    </section>
+    <br>
+    <section id="banner">
+    <h1 id="texto-principal"><b>O lugar certo para abastecer seu veículo!
+        Consulte nossas unidades.</b>
+    </h1>
+    <br>
+    <img src="img/postodegasolina.jpeg">
+    </section>
+    <br>
+    <div class="carrosselimg">
+    <h1 id="texto-app"> Baixe nosso app! Cupons de desconto e delivery </h1>
+    <img src="https://www.gov.br/pt-br/imagens-de-servicos/apple.png" id="apple">
+    </div>
+    <div class="carrossel">
+        <div class="container" id="img">
+            <img src="img/promo.png" alt="">
+            <img src="img/melhor-gasolina.png" alt="">
+            <img src="img/delivery.png" alt="">
+        </div>
+</body>
+</html>
