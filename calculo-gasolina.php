d7ad6057fd4a0f6b45b04e615c94731c804f2f6d
nikolasmiranda <129020080+nikolasmiranda@users.noreply.github.com>
2023-11-28 16:29:11 -0300

    Projeto Final
    Esse é o commit do projeto finalizado.

--- calculo-gasolina.php
+++ calculo-gasolina.php
@@ -3,13 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projeto</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Posto Express</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="script.js" defer></script>
 </head>
 <body>
+    <section id=navegacao>
+    <nav>
+        <ul id="ulnav">
+            <li class="navli"><a class="ancoragemnav" href="index.php">Início</a></li>
+            <li class="navli"><a class="ancoragemnav" href="calculo-gasolina.php">Calculo de Gasolina</a></li>
+            <li class="navli"><a class="ancoragemnav" href="unidades.html">Unidades</a></li>
+            <li class="navli"><a class="ancoragemnav" href="sobre.html">Sobre</a></li>
+            <li class="navli"><a class="ancoragemnav" href="formulario_contato.html">Contato</a></li>
+        </ul>
+    </nav>
+    </section>
     <main>
     <div class="painel">
-        <h2>Os combustíveis disponíveis em nosso posto são: </h2>
+        <h2 id="h2-painel">Os combustíveis disponíveis em nosso posto são: </h2>
         <div class="conteudo-painel">
             <p>O posto Express destaca-se pela qualidade excepcional de seus combustíveis, mantendo padrões rigorosos. 
                 Apesar das variações nos preços devido a fatores externos, o posto é transparente ao fornecer informações atualizadas. 
@@ -36,5 +48,49 @@
     </div>
 
     </main>
+    <br>
+    <section id="footers">
+        <section class="rodapes1">
+        <footer>
+            <h1>Páginas</h1>
+            <ul>
+                <li><a href="index.php">Início</a></li>
+                <li><a href="calculo-gasolina.php">Cálculo de Gasolina</a></li>
+                <li><a href="unidades.html">Unidades</a></li>
+                <li><a href="unidades.html">sobre</a></li>
+                <li><a href="formulario-contato.html">Contato</a></li>
+            </ul>
+        </footer>
+        </section>
+        <section class="rodapes2">
+            <footer>
+                <h1>Redes Sociais</h1>
+                <ul>
+                    <li><a href="instagram.com">Instagram</a></li>
+                    <li><a href="twitter.com">X(Twitter)</a></li>
+                    <li><a href="twitter.com">Youtube</a></li>
+                    <li><a href="web.whatsapp.com">Whatsapp</a></li>
+                </ul>
+            </footer>
+        </section>
+        <section class="rodapes1">
+        <footer>
+            <h1>Delivery</h1>
+            <ul>
+                <li><a href="web.whatsapp.com">Whatsapp</a></li>
+                <li><a href="ifood.com.br">Ifood</a></li>
+                <li><a href="rappi.com.br">Rappi</a></li>
+                <li><a href="uber.com">Uber Eats</a></li>
+            </ul>
+        </footer>
+        </section>
+        <section class="rodapes2"> 
+            <h1>Membros da Equipe</h1>
+            <ul>
+                <li><a href="https://github.com/nikolasmiranda">Nikolas Gonçalves Miranda</a></li>
+                <li><a href="Luccagm1">Lucca Gonçalves Miranda</a></li>
+            </ul>
+        </section>
+    </section>
 </body>
 </html>
--- calculo.php
+++ calculo.php
@@ -36,9 +36,22 @@ if ($_POST) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculo Consumo de Combustivel</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Posto Express</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="script.js" defer></script>
 </head>
+<body>
+    <section id=navegacao>
+    <nav>
+        <ul id="ulnav">
+            <li class="navli"><a class="ancoragemnav" href="index.php">Início</a></li>
+            <li class="navli"><a class="ancoragemnav" href="calculo-gasolina.php">Calculo de Gasolina</a></li>
+            <li class="navli"><a class="ancoragemnav" href="unidades.html">Unidades</a></li>
+            <li class="navli"><a class="ancoragemnav" href="sobre.html">Sobre</a></li>
+            <li class="navli"><a class="ancoragemnav" href="formulario_contato.html">Contato</a></li>
+        </ul>
+    </nav>
+    </section>
 <body>
     <main>
         <div class="painel">
@@ -51,5 +64,49 @@ if ($_POST) {
             </div>
         </div>
     </main>
+    <br>
+    <section id="footers">
+        <section class="rodapes1">
+        <footer>
+            <h1>Páginas</h1>
+            <ul>
+                <li><a href="index.php">Início</a></li>
+                <li><a href="calculo-gasolina.php">Cálculo de Gasolina</a></li>
+                <li><a href="unidades.html">Unidades</a></li>
+                <li><a href="unidades.html">sobre</a></li>
+                <li><a href="formulario-contato.html">Contato</a></li>
+            </ul>
+        </footer>
+        </section>
+        <section class="rodapes2">
+            <footer>
+                <h1>Redes Sociais</h1>
+                <ul>
+                    <li><a href="instagram.com">Instagram</a></li>
+                    <li><a href="twitter.com">X(Twitter)</a></li>
+                    <li><a href="twitter.com">Youtube</a></li>
+                    <li><a href="web.whatsapp.com">Whatsapp</a></li>
+                </ul>
+            </footer>
+        </section>
+        <section class="rodapes1">
+        <footer>
+            <h1>Delivery</h1>
+            <ul>
+                <li><a href="web.whatsapp.com">Whatsapp</a></li>
+                <li><a href="ifood.com.br">Ifood</a></li>
+                <li><a href="rappi.com.br">Rappi</a></li>
+                <li><a href="uber.com">Uber Eats</a></li>
+            </ul>
+        </footer>
+        </section>
+        <section class="rodapes2"> 
+            <h1>Membros da Equipe</h1>
+            <ul>
+                <li><a href="https://github.com/nikolasmiranda">Nikolas Gonçalves Miranda</a></li>
+                <li><a href="Luccagm1">Lucca Gonçalves Miranda</a></li>
+            </ul>
+        </section>
+    </section>
 </body>
 </html>
